117d3f0fb646481a37181791e93dbaf2e45eff77
Shamiratan G Sah <visitingcardorder2@gmail.com>
2021-09-01 15:06:22 +0530

    first commit

---
+++ 17_writing_Html_within_php.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '<h1> Demo of Using HTML Element within PHP </h1>';
+echo '<h2> Demo of Using HTML Element within PHP </h2>';
+echo '<h3> Demo of Using HTML Element within PHP </h3>';
+echo '<h4> Demo of Using HTML Element within PHP </h4>';
+echo '<h5> Demo of Using HTML Element within PHP </h5>';
+echo '<h6> Demo of Using HTML Element within PHP </h6>';
+
+
+echo '<select> <option> Select Option </option>  <option> Option One </option> <option> Option Two </option> </select>';
+
+
+echo '<br><br><input type="text" value="Text One">';
+
+echo '<br><br><textarea> Lots of Content in Textarea </textarea>';
+
+echo '<br><br> <table border="2px" style="color:white; background:green;"> <tr> <td style="padding:20px;"> Column One </td> <td style="padding: 20px;"> Column Two </td> <td style="padding:20px;"> Column Three </td> </tr> </table>'
+
+
+?>
